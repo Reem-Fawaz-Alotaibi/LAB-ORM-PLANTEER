9c1dd4442c8566cf4131b01e1e74ecef7cbaf663
Reem <reemfawazalotaibi11@gmail.com>
2026-04-26 08:08:33 +0300

    Adding the email part

--- Planteer/Planteer/Planteer/settings.py
+++ Planteer/Planteer/Planteer/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -120,3 +122,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
--- Planteer/Planteer/contact/views.py
+++ Planteer/Planteer/contact/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.contrib import messages
 from .forms import ContactForm
 from .models import Contact
 
@@ -6,14 +10,27 @@ from .models import Contact
 def contact_view(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
+
         if form.is_valid():
-            form.save()
+            contact = form.save()
+            content_html = render_to_string('contact/email.html', {
+                'name': contact.first_name,
+                'message': contact.message,
+            })
+            email = EmailMessage(
+                subject='We received your message - Planteer 🌱',
+                body=content_html,
+                from_email=settings.EMAIL_HOST_USER,
+                to=[contact.email],
+            )
+            email.content_subtype = 'html' 
+            email.send()
+            messages.success(request,"your message is received")
             return redirect('contact:contact')
     else:
         form = ContactForm()
 
     return render(request, 'contact/contact.html', {'form': form})
-
 
 def contact_messages_view(request):
     messages = Contact.objects.all().order_by('-created_at')
